773bdd5b40cd5a1aad12788c037c847e3c459b6f
jackhexed <jackdotink@gmail.com>
2026-09-21 15:14:27 -0600

    remove mutable vscode settings

--- modules/vscode/home.nix
+++ modules/vscode/home.nix
@@ -5,8 +5,6 @@
 
 {
   programs.vscode.enable = true;
-  programs.vscode.mutableExtensionsDir = true;
-  programs.vscode.profiles.Default.mutableUserSettings = true;
   programs.vscode.argvSettings = {
     "password-store" = "gnome-libsecret";
   };
