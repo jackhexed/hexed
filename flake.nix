18e4532cd4f6650884b95ca737246993ee5c7df8
jackhexed <jackdotink@gmail.com>
2026-09-21 02:54:02 -0600

    fix out of place inputs

--- flake.nix
+++ flake.nix
@@ -1,6 +1,24 @@
 {
   description = "A flake for my personal NixOS configuration";
 
-  inputs = import ./inputs.nix;
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    umbriel = {
+      url = "github:noctalia-dev/umbriel";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    zen-browser = {
+      url = "github:youwen5/zen-browser-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
   outputs = import ./outputs.nix;
 }
